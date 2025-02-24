57d0004714073e2cd0f10c4911d77ef9def5aee0
Rustem <rustem_hajrullin@enot.work>
2025-02-24 23:00:25 +0300

    feat: added team and athletes swipers and cards

--- js/swipers.js
+++ js/swipers.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('#mainSwiper', {
+var mainSwiper = new Swiper('#mainSwiper', {
   navigation: {
     nextEl: '#main-swiper-next-button',
     prevEl: '#main-swiper-prev-button',
@@ -11,6 +11,55 @@ var swiper = new Swiper('#mainSwiper', {
   //   delay: 2500,
   //   disableOnInteraction: false,
   // },
+  spaceBetween: 20,
+  effect: 'slide',
+  speed: 1000,
+});
+
+var teamSwiper = new Swiper('#teamSwiper', {
+  spaceBetween: 15,
+  slidesPerView: 4,
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+  effect: 'slide',
+  speed: 1000,
+});
+
+var teamSwiper = new Swiper('#athletesSwiper', {
+  spaceBetween: 15,
+  slidesPerView: 4,
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
   effect: 'slide',
   speed: 1000,
 });
